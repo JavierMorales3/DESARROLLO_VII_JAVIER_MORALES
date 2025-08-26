c1d6d6b790b51f6f178a2347da9645e08c832893
JavierMorales3 <javier.morales1@utp.ac.pa>
2025-08-25 19:40:48 -0500

    Ejercicio de perfil de usuario completado

---
+++ TALLERES/TALLER_2/perfil_usuario.php
@@ -0,0 +1,33 @@
+
+<?php
+// Definición de variables
+$nombre = "Javier Morales";
+$edad = 22;
+$correo = "javier.morales@utp.ac.pa";
+$telefono = "503-4264-4729";
+
+// Definición de constante
+define("OCUPACION", "Estudiante de Desarrollo de Software");
+
+// Creación de mensaje usando diferentes métodos de concatenación e impresión
+$mensaje1 = "Hola, mi nombre es " . $nombre . " y tengo " . $edad . " años.";
+$mensaje2 = "Mi correo es $correo y soy " . OCUPACION . ".";
+
+echo $mensaje1 . "<br>";
+print($mensaje2 . "<br>");
+
+printf("En resumen: %s, %d años, %s, %s<br>", $nombre, $edad, $cocrreo, OCUPACION);
+
+echo "<br>Información de debugging:<br>";
+var_dump($nombre);
+echo "<br>";
+var_dump($edad);
+echo "<br>";
+var_dump($correo);
+echo "<br>";
+var_dump($telefono);
+echo "<br>";
+var_dump(OCUPACION);
+echo "<br>";
+?>
+                    
