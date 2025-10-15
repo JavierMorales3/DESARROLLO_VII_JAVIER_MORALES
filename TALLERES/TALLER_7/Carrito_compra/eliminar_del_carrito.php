4f06071778b7bdbf4fcd3b43da36e3cf3d570d34
JavierMorales3 <142769105+JavierMorales3@users.noreply.github.com>
2025-10-15 09:49:22 -0500

    TALLER_7 - Implementación del sistema de carrito de compras

--- TALLERES/TALLER_7/Carrito_compra/eliminar_del_carrito.php
+++ TALLERES/TALLER_7/Carrito_compra/eliminar_del_carrito.php
@@ -1,6 +1,14 @@
 <?php
 include 'config_sesion.php';
+
 if (isset($_GET['id'])) {
-    $id_producto =  filter_input(INPUT_POST, 'id_producto', FILTER_VALIDATE_INT);
+    $id_producto = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+
+    if ($id_producto !== false && $id_producto !== null && isset($_SESSION['carrito'][$id_producto])) {
+        unset($_SESSION['carrito'][$id_producto]);
+    }
 }
+
+header('Location: ver_carrito.php');
+exit;
 ?>
